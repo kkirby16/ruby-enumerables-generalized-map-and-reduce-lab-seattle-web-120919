ca6fe16b475ddd00ef36ac17603c9f91f78df25d
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-12-03 07:57:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ def reduce(array, starting_point=0)
  end 
     #now want to loop over array so we can return the sum of these values.
     while i < array.length do 
-      
+      yield #pass each elemetn to this block
   
 
 end 
