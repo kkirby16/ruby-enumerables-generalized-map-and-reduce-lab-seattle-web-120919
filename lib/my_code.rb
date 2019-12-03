427cb51d2da7c8db2eaec6ec0a49b1b02d5fa492
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-12-03 08:01:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,8 +11,8 @@ end
 
 def reduce(array, starting_point=0)
  if starting_point #if there is a starting point
-   sum = starting_point  #will set num1 equal to that starting value 
-   i = 0 #then add a counter that starts at first element in our array.
+   sum = starting_point  #will set num1 equal to that starting #value 
+   i = 0 #then add a counter that starts at first element in our #array.
   else sum = array[0] #if no starting value then our num1 will be #set to the first number in our array so 0 is the index #indicating we will be setting num1 equal to that first #element.
    i = 1 #since we are already working with the first element so #we want to move on to the next element after above line was #executed
  end 
