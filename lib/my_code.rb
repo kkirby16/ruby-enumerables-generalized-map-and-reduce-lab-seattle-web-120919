41f664dec05fa11108c31b99fbd47d4321deb850
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-12-03 06:56:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
-# Your Code Here
+def map(array)
+new = []
+i = 0 
+while i < array.length do 
+  new.push(yield(array[i])) #want to return a new array so need to #add these manipulated elements manipulated by our block to a #new array so use the push method.
+  i += 1 
+  
+end
+new 
+end 
+
+map(array){|array| array[i] * -1}
+ 
+
+ 
+ 
